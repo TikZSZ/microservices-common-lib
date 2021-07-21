ff598ceaa2a62bb27c9449794552a00d3cce5b63
Aditya <repackergame@gmail.com>
2021-07-21 07:17:35 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { CustomCookieSessionObject } from "./interface/CookieSessonObject"
 import { UserPayload } from "./interface/UserPayload"
 
-export * from "./errors/bad-request-error"
 export * from "./errors/custom-error"
+export * from "./errors/bad-request-error"
 export * from "./errors/database-connection-error"
 export * from "./errors/not-authorized-error"
 export * from "./errors/not-found-error"
 export * from "./errors/request-validation-error"
 
-export * from "./middlewares/current-user"
 export * from "./middlewares/error-handler" 
+export * from "./middlewares/current-user"
 export * from "./middlewares/require-auth" 
 export * from "./middlewares/validate-request" 
 
