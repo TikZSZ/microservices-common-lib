48569e4dd448fd265211cb0563a98480ebee02e9
Aditya <repackergame@gmail.com>
2021-08-08 03:46:54 +0530

    Updates

--- src/events/events-interface/index.ts
+++ src/events/events-interface/index.ts
@@ -2,4 +2,4 @@ export * from "./order-created-event"
 export * from "./order-cancelled-event"
 export * from "./ticket-created-event"
 export * from "./ticket-updated-event"
-
+export * from "./expiration-complete-event"
